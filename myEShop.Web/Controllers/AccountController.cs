87727a5c8dd5b34033b0f1eebb058afeca2898f7
Mostafa Rasouli <Kasraco@gmail.com>
2024-04-06 22:27:35 +0330

    change Old Login system to new with signinmanager

--- myEShop.Web/Controllers/AccountController.cs
+++ myEShop.Web/Controllers/AccountController.cs
@@ -15,57 +15,65 @@ public class AccountController : Controller
 
     private readonly myEShopContext _context;
     private readonly UserManager<IdentityUser> _userManager;
-    public AccountController(UserManager<IdentityUser> userManager)
+    private readonly SignInManager<IdentityUser> _signInManager;
+    public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
     {
         _userManager = userManager;
+        _signInManager = signInManager;
     }
 
     [HttpGet]
     public IActionResult Login(string? ReturnUrl = null)
     {
-        ViewBag.ReturnUrl = ReturnUrl;
+        ViewData["ReturnUrl"] = ReturnUrl;
         return View();
     }
 
 
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public IActionResult Login(LoginViewModel model, string? ReturnUrl = null)
+    public async Task<IActionResult> Login(LoginViewModel model, string? ReturnUrl = null)
     {
+        ViewData["ReturnUrl"] = ReturnUrl;
         if (!ModelState.IsValid)
             return View(model);
 
-        var user = GetUser(model.Email.ToLower(), model.Password);
-        if (user == null)
+
+        var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+        if (result.Succeeded)
         {
-            ModelState.AddModelError("Email", "The information entered is not correct");
-            return View(model);
-        }
-
-        var role = user.IsAdmin ? "Admin" : "User";
-        var claims = new List<Claim>
-        {
-            new Claim(ClaimTypes.NameIdentifier,user.UserId.ToString()),
-            new Claim(ClaimTypes.Name,user.Email),
-            new Claim(ClaimTypes.Role,role)
-        };
-
-        var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-        var claimsprincipal = new ClaimsPrincipal(identity);
-
-        var properties = new AuthenticationProperties
-        {
-            IsPersistent = model.RememberMe
-        };
-        HttpContext.SignInAsync(claimsprincipal, properties);
-
-        if (!string.IsNullOrEmpty(ReturnUrl))
-            if (Url.IsLocalUrl(ReturnUrl))
-                return Redirect(ReturnUrl);
+            if (!string.IsNullOrEmpty(ReturnUrl))
+                if (Url.IsLocalUrl(ReturnUrl))
+                    return Redirect(ReturnUrl);
+                else
+                    return Redirect("/");
             else
                 return Redirect("/");
+
+        }
         else
-            return Redirect("/");
+        {
+            ModelState.AddModelError(string.Empty, "Invalid login attemp");
+        }
+
+        // var role = user.IsAdmin ? "Admin" : "User";
+        // var claims = new List<Claim>
+        // {
+        //     new Claim(ClaimTypes.NameIdentifier,user.UserId.ToString()),
+        //     new Claim(ClaimTypes.Name,user.Email),
+        //     new Claim(ClaimTypes.Role,role)
+        // };
+
+        // var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        // var claimsprincipal = new ClaimsPrincipal(identity);
+
+        // var properties = new AuthenticationProperties
+        // {
+        //     IsPersistent = model.RememberMe
+        // };
+        // HttpContext.SignInAsync(claimsprincipal, properties);
+
+        return View(model);
     }
 
     [HttpGet]
@@ -104,10 +112,12 @@ public class AccountController : Controller
     }
 
     [HttpPost]
-    public IActionResult Logout()
+    public async Task<IActionResult> Logout()
     {
-        HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-        return Redirect("/Account/Login");
+        // HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        await _signInManager.SignOutAsync();
+
+        return RedirectToAction(nameof(AccountController.Login), "Account");
     }
 
     [HttpGet]
